f3d99fe4104a4873aec32fa23998d263604594ec
AHNYUNKI <ahnyunki@naver.com>
2024-01-12 16:23:07 +0900

    알고리즘 - 선택 정렬

--- src/algorithm/sort/Sort.java
+++ src/algorithm/sort/Sort.java
@@ -18,20 +18,55 @@ public class Sort {
         }
     }
 
-    public static void main(String[] args) {
-        int[] arr = {4, 2, 3, 1};
+    /**
+     * 선택 정렬
+     * 구현은 간단하나, 성능이 좋지 못하다. O(n^2)
+     */
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minValueIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minValueIndex]) {
+                    minValueIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minValueIndex];
+            arr[minValueIndex] = temp;
+        }
+    }
 
-        System.out.println("===== 정렬 전 =====");
-        for (int a : arr) {
+    public static void main(String[] args) {
+        int[] bubble = {4, 2, 3, 1};
+
+        System.out.println("===== 버블 정렬 전 =====");
+        for (int a : bubble) {
             System.out.print(a + " ");
         }
 
-        bubbleSort(arr);
+        bubbleSort(bubble);
         System.out.println();
 
-        System.out.println("===== 정렬 후 =====");
-        for (int a : arr) {
+        System.out.println("===== 버블 정렬 후 =====");
+        for (int a : bubble) {
             System.out.print(a + " ");
         }
+        System.out.println();
+
+        int[] selection = {4, 2, 3, 1};
+
+        System.out.println("===== 선택 정렬 전 =====");
+        for (int a : selection) {
+            System.out.print(a + " ");
+        }
+
+        selectionSort(selection);
+        System.out.println();
+
+        System.out.println("===== 선택 정렬 후 =====");
+        for (int a : selection) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
     }
 }
