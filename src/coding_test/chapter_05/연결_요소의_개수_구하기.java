ebe703c6dec3e48be2eabb14bde8b7a02b573e61
ahnyunki <ahnyunki00@gmail.com>
2024-02-22 21:19:23 +0900

    Do it - 연결 요소의 개수 구하기

---
+++ src/coding_test/chapter_05/연결_요소의_개수_구하기.java
@@ -0,0 +1,54 @@
+package coding_test.chapter_05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class 연결_요소의_개수_구하기 {
+
+    static ArrayList<Integer>[] A;
+    static boolean visited[];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[n+1];
+        A = new ArrayList[n+1];
+        for (int i = 0; i < n+1; i++) {
+            A[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken()); // 시작점
+            int e = Integer.parseInt(st.nextToken()); // 종료
+            A[s].add(e);
+            A[e].add(s);
+        }
+        int count = 0;
+        for (int i = 0; i < n+1; i++) {
+            if (!visited[i]) {
+                count++;
+                DFS(i);
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static void DFS(int v) {
+        if (visited[v]) {
+            return;
+        }
+        visited[v] = true;
+        for (int i : A[v]) {
+            if (!visited[i]) {
+                DFS(i);
+            }
+        }
+    }
+}
